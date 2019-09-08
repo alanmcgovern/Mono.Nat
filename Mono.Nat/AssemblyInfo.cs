7396ddb6b33fdb1188c1f53538c1a0eebe07ecb9
Alan McGovern <alan@xamarin.com>
2019-09-08 16:24:36 +0100

    [build] Prepare a 2.0 release

--- Mono.Nat/AssemblyInfo.cs
+++ Mono.Nat/AssemblyInfo.cs
@@ -3,7 +3,6 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyDescription (".NET Library for automatic network address translation")]
-[assembly: AssemblyCopyright ("Copyright Alan McGovern, Ben Motmans ©  2006-2007")]
 [assembly: ComVisible (false)]
 
 [assembly: CLSCompliant (true)]
